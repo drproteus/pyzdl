d88d7f2b4145621167b8729b9b57f8ae0a6812ca
Jake Goritski <jgoritski@headspin.io>
2024-10-11 16:36:35 -0700

    setup click and parse test data config

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-import wx
+import json
+import click
+from dataclasses import dataclass
+from lib.models import SourcePort, Profile
 
 
-def main():
-    app = wx.App()
-    frame = wx.Frame(None, title="pyzdl")
-    panel = wx.Panel(frame)
-    text = wx.StaticText(panel, label="Hello, underworld.")
-    frame.Show()
-    app.MainLoop()
+@dataclass
+class LoaderApp:
+    source_ports: dict[str, SourcePort]
+    profiles: dict[str, Profile]
+
+
+def setup():
+    source_ports = {}
+    with open("./test/data.json", "r") as f:
+        data_json = json.load(f)
+    source_ports_json = data_json["source_ports"]
+    for source_port_json in source_ports_json:
+        source_ports[source_port_json["name"]] = SourcePort.from_json(source_port_json)
+    profiles_json = data_json["profiles"]
+    profiles = {}
+    for profile_json in profiles_json:
+        # Update JSON with shared source_port mapping.
+        profile_json["port"] = source_ports[profile_json["port"]].to_json()
+        profiles[profile_json["name"]] = Profile.from_json(profile_json)
+    return LoaderApp(
+        source_ports=source_ports,
+        profiles=profiles,
+    )
+
+
+@click.command("pyzdl")
+@click.option("--profile", "profile_name", type=click.STRING)
+def main(profile_name):
+    app = setup()
+    if not profile_name:
+        profile_name = next(iter(app.profiles.keys()))
+    profile = app.profiles[profile_name]
+    profile.launch()
 
 
 if __name__ == "__main__":
