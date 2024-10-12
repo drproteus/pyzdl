005ab72fce2d71adb8ab47b463c55f87406130df
Jake Goritski <jgoritski@headspin.io>
2024-10-11 17:22:19 -0700

    reorganize commands and add ls subcommands

--- main.py
+++ main.py
@@ -9,19 +9,32 @@ def setup(config_path):
     return LoaderApp.from_json(data_json)
 
 
-@click.command("pyzdl")
+def default_options(fn):
+    @click.option(
+        "--config",
+        "config_path",
+        default="./test/data.json",
+        type=click.Path(exists=True),
+    )
+    def wrapped(config_path, *args, **kwargs):
+        app = setup(config_path)
+        return fn(app, config_path, *args, **kwargs)
+    return wrapped
+
+
+@click.group("pyzdl")
+def pyzdl():
+    pass
+
+
+@pyzdl.command("run")
 @click.option(
     "--profile",
     "profile_name",
     type=click.STRING,
 )
-@click.option(
-    "--config",
-    "config_path",
-    default="./test/data.json",
-    type=click.Path(exists=True),
-)
-def main(profile_name, config_path):
+@default_options
+def run(app, config_path, profile_name):
     app = setup(config_path=config_path)
     if not app.profiles:
         raise click.ClickException("No available profiles.")
@@ -34,5 +47,19 @@ def main(profile_name, config_path):
     profile.launch()
 
 
+@pyzdl.command("ls-profiles")
+@default_options
+def ls_profiles(app, config_path):
+    for profile_name, profile in app.profiles.items():
+        click.echo(f"{profile_name} ({profile.port.name})")
+
+
+@pyzdl.command("ls-ports")
+@default_options
+def ls_ports(app, config_path):
+    for source_port_name, source_port in app.source_ports.items():
+        click.echo(f"{source_port_name} ({source_port.executable.path})")
+
+
 if __name__ == "__main__":
-    main()
+    pyzdl()
