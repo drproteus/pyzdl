144fef96d73e9f87d9da8c8cee4622087cff1d8d
Jake Goritski <jgoritski@headspin.io>
2024-10-11 18:31:58 -0700

    get config from home, log available profiles and config_path

--- lib/util.py
+++ lib/util.py
@@ -1,12 +1,22 @@
 import json
 import click
+import os
+from pathlib import Path
 from lib.models import LoaderApp
 
 
+CONFIG_PATH = Path.home().joinpath(".config", "pyzdl", "config.json")
+
+
 def setup(config_path):
+    click.echo(f"Loading config from {config_path}", err=True)
     with open(config_path, "r") as f:
         data_json = json.load(f)
-    return LoaderApp.from_json(data_json)
+    app = LoaderApp.from_json(data_json)
+    click.echo("Available profiles:")
+    for name, _ in app.profiles.items():
+        click.echo(f"* {name}", err=True)
+    return app
 
 
 def write_config(app, config_path):
@@ -18,7 +28,7 @@ def default_options(fn):
     @click.option(
         "--config",
         "config_path",
-        default="./test/data.json",
+        default=CONFIG_PATH,
         type=click.Path(exists=True),
     )
     def wrapped(config_path, *args, **kwargs):
