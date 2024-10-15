4cba4c93398ac2dfc70b52eec9fc3d39239df518
Jake Goritski <jgoritski@headspin.io>
2024-10-14 22:55:03 -0700

    add/edit profile dialog

--- gui.py
+++ gui.py
@@ -2,7 +2,124 @@ import wx
 import os
 import click
 from lib.util import default_options, setup, write_config
-from wxglade.wxglade_out import MainWindow
+from lib.models import Profile, Resource
+from wxglade.wxglade_out import MainWindow, AddProfileDialogWindow
+
+
+class AddProfileDialog(AddProfileDialogWindow):
+    def __init__(self, parent, profile=None):
+        super().__init__(parent=parent)
+        self.dirname = "."
+        self.parent = parent
+        self.app = parent.app
+        self.profile = profile
+        self.add_profile_iwad_choice_list.Clear()
+        self.iwad_choices = []
+        self.source_port_choices = []
+        if self.profile:
+            self.add_profile_name.Value = self.profile.name
+        for i, (iwad_name, _) in enumerate(self.app.iwads.items()):
+            self.iwad_choices.append(iwad_name)
+            self.add_profile_iwad_choice_list.Append(iwad_name)
+            if self.profile and self.profile.iwad.name == iwad_name:
+                self.add_profile_iwad_choice_list.SetSelection(i)
+        self.add_profile_source_port_choice_list.Clear()
+        for i, (port_name, _) in enumerate(self.app.source_ports.items()):
+            self.source_port_choices.append(port_name)
+            self.add_profile_source_port_choice_list.Append(port_name)
+            if self.profile and self.profile.port.name == port_name:
+                self.add_profile_source_port_choice_list.SetSelection(i)
+        self.filenames = []
+        self.add_profile_files_list_box.Clear()
+        if self.profile and self.profile.files:
+            for file in self.profile.files:
+                self.add_profile_files_list_box.Append(file.path)
+                self.filenames.append(file.path)
+        self.confirm_add_profile_button.Bind(wx.EVT_LEFT_UP, self.on_confirm)
+        self.add_profile_cancel_button.Bind(wx.EVT_LEFT_UP, self.on_cancel)
+        self.remove_profile_file_button.Bind(wx.EVT_LEFT_UP, self.remove_file)
+        self.add_profile_file_move_up_button.Bind(wx.EVT_LEFT_UP, self.move_file_up)
+        self.add_profile_file_move_down_button.Bind(wx.EVT_LEFT_UP, self.move_file_down)
+        self.add_profile_file_button.Bind(wx.EVT_LEFT_UP, self.on_file_add)
+
+    def refresh_files(self):
+        self.add_profile_files_list_box.Clear()
+        for path in self.filenames:
+            self.add_profile_files_list_box.Append(path)
+
+    def on_confirm(self, e):
+        if not self.add_profile_name.Value.strip():
+            return
+        profile = Profile(
+            name=self.add_profile_name.Value,
+            iwad=self.app.iwads[
+                self.iwad_choices[self.add_profile_iwad_choice_list.Selection]
+            ],
+            port=self.app.source_ports[
+                self.source_port_choices[
+                    self.add_profile_source_port_choice_list.Selection
+                ]
+            ],
+            files=[Resource(path=path) for path in self.filenames],
+            args=self.add_profile_args_box.Value,
+        )
+        self.app.profiles[profile.name] = profile
+        self.parent.refresh()
+        self.Destroy()
+
+    def on_cancel(self, e):
+        self.Destroy()
+
+    def get_selected_filename(self):
+        return self.add_profile_files_list_box.GetStringSelection()
+
+    def remove_file(self, e):
+        path = self.get_selected_filename()
+        self.filenames.remove(path)
+        self.refresh_files()
+
+    def move_file_up(self, e):
+        path = self.get_selected_filename()
+        index = self.filenames.index(path)
+        if len(self.filenames) < 2:
+            return
+        if index == 0:
+            return
+        self.filenames[index], self.filenames[index - 1] = (
+            self.filenames[index - 1],
+            self.filenames[index],
+        )
+        self.add_profile_files_list_box.SetSelection(index - 1)
+        self.refresh_files()
+
+    def move_file_down(self, e):
+        path = self.get_selected_filename()
+        index = self.filenames.index(path)
+        if len(self.filenames) < 2:
+            return
+        if index == len(self.filenames) - 1:
+            return
+        self.filenames[index], self.filenames[index + 1] = (
+            self.filenames[index + 1],
+            self.filenames[index],
+        )
+        self.add_profile_files_list_box.SetSelection(index + 1)
+        self.refresh_files()
+
+    def on_file_add(self, e):
+        dialog = wx.FileDialog(
+            self,
+            "Choose a file",
+            self.dirname,
+            "",
+            "*.wad;*.pk3",
+            wx.FD_OPEN,
+        )
+        if dialog.ShowModal() == wx.ID_OK:
+            path = os.path.join(dialog.GetDirectory(), dialog.GetFilename())
+            self.filenames.append(path)
+            self.refresh_files()
+        dialog.Destroy()
 
 
 class MainFrame(MainWindow):
@@ -21,7 +138,9 @@ class MainFrame(MainWindow):
         menu_exit = filemenu.Append(wx.ID_EXIT, "&Exit", " Terminate the program")
         menu_load_config = filemenu.Append(wx.ID_FILE1, "&Load", " Load new config")
         menu_reload = filemenu.Append(wx.ID_FILE2, "&Reload", " Reload config")
-        menu_save = filemenu.Append(wx.ID_FILE3, "&Save", " Save profile args to config")
+        menu_save = filemenu.Append(
+            wx.ID_FILE3, "&Save", " Save profile args to config"
+        )
 
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "&File")
@@ -50,6 +169,9 @@ class MainFrame(MainWindow):
         self.profiles_list_box.Bind(wx.EVT_LISTBOX, self.on_update)
         self.profiles_list_box.Bind(wx.EVT_LISTBOX_DCLICK, self.on_click)
         self.args_box.Bind(wx.EVT_TEXT, self.on_args_edit)
+        self.add_profile_button.Bind(wx.EVT_LEFT_UP, self.show_add_profile_dialog)
+        self.edit_profile_button.Bind(wx.EVT_LEFT_UP, self.show_edit_profile_dialog)
+        self.remove_profile_button.Bind(wx.EVT_LEFT_UP, self.remove_profile)
 
         self.profiles_list_box.Selection = 0
         self.on_update(None)
@@ -117,6 +239,11 @@ class MainFrame(MainWindow):
             profile.launch()
         dialog.Destroy()
 
+    def refresh(self):
+        new_frame = MainFrame(self.app, self.config_path)
+        new_frame.Show()
+        self.Destroy()
+
     def reload(self, config_path):
         app = setup(config_path)
         new_frame = MainFrame(app, config_path)
@@ -143,14 +270,31 @@ class MainFrame(MainWindow):
     def on_save_config(self, e):
         write_config(self.app, self.config_path)
 
+    def show_add_profile_dialog(self, e):
+        add_profile_dialog = AddProfileDialog(self)
+        add_profile_dialog.Show()
+
+    def show_edit_profile_dialog(self, e):
+        edit_profile_dialog = AddProfileDialog(
+            self, profile=self.get_selected_profile()
+        )
+        edit_profile_dialog.Show()
+
+    def remove_profile(self, e):
+        selected_profile_name = self.profiles_list_box.GetStringSelection()
+        del self.app.profiles[selected_profile_name]
+        self.refresh()
+
 
 @click.command("pyzdl_gui")
+@click.option("--verbose", "-v", is_flag=True, default=False)
 @default_options
-def main(app, config_path):
-    click.echo(f"Loading config from {config_path}", err=True)
-    click.echo("Available profiles:")
-    for name, _ in app.profiles.items():
-        click.echo(f"* {name}", err=True)
+def main(app, config_path, verbose):
+    if verbose:
+        click.echo(f"Loading config from {config_path}", err=True)
+        click.echo("Available profiles:")
+        for name, _ in app.profiles.items():
+            click.echo(f"* {name}", err=True)
     gui = wx.App()
     frame = MainFrame(app, config_path)
     gui.MainLoop()
